ee2688491b0b41fb0e96235e8922b098c4fefc5b
Josh Hills <joshuamarcushills@gmail.com>
2021-01-24 19:45:52 +0100

    fix(): site url env var

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,16 +1,17 @@
 import { buildConfig } from 'payload/config';
+import dotenv from 'dotenv';
 import Media from './collections/Media';
 import TodoLists from './collections/TodoLists';
 import Users from './collections/Users';
 
 console.log(process.cwd());
 console.log(process.env['SITE_URL']);
-console.log('ah');
-require('dotenv').config();
+console.log('ah'); 
+dotenv.config();
 console.log(process.env['SITE_URL']);
 
 export default buildConfig({
-  serverURL: process.env.SITE_URL,
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
     user: Users.slug,
   },
