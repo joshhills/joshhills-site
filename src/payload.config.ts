f3e7964cb1a2088d00bd1c5bad0aad1b69dde948
Josh Hills <joshuamarcushills@gmail.com>
2021-01-24 08:07:47 +0100

    fix(): hack for site url

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,6 @@ import Media from './collections/Media';
 import TodoLists from './collections/TodoLists';
 import Users from './collections/Users';
 
-console.log(process.env.SITE_URL);
-
 export default buildConfig({
   serverURL: process.env.SITE_URL,
   admin: {
