0bc0e7af5df397abf81606dd898eca055d87f77e
Josh Hills <joshuamarcushills@gmail.com>
2021-01-24 09:01:11 +0100

    chore(): logging

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,11 @@ import Media from './collections/Media';
 import TodoLists from './collections/TodoLists';
 import Users from './collections/Users';
 
+console.log(process.cwd());
+console.log(process.env['SITE_URL']);
+console.log('ah');
 require('dotenv').config();
+console.log(process.env['SITE_URL']);
 
 export default buildConfig({
   serverURL: process.env.SITE_URL,
