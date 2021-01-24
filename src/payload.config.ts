d0c901cf70ff44a49a997bb010b8fef7fa4858c4
Josh Hills <joshuamarcushills@gmail.com>
2021-01-24 19:57:45 +0100

    chore(): console log

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,10 +5,10 @@ import TodoLists from './collections/TodoLists';
 import Users from './collections/Users';
 
 console.log(process.cwd());
-console.log(process.env['SITE_URL']);
+console.log(process.env['PAYLOAD_PUBLIC_SERVER_URL']);
 console.log('ah'); 
 dotenv.config();
-console.log(process.env['SITE_URL']);
+console.log(process.env['PAYLOAD_PUBLIC_SERVER_URL']);
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
