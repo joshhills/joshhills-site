bb0b0d281d5a5796eb3c4c69a59235bf73b1724f
Josh Hills <joshuamarcushills@gmail.com>
2021-01-26 02:07:55 +0100

    fix: env mongo script

--- init-mongo.sh
+++ init-mongo.sh
@@ -2,14 +2,14 @@ set -e
 
 mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin<<EOF
 
-use payload-$NODE_ENV
+use payload-$ENVIRONMENT
 
 db.createUser({
   user: '$MONGODB_USERNAME',
   pwd: '$MONGODB_PASSWORD',
   roles: [{
     role: 'readWrite',
-    db: 'payload-$NODE_ENV'
+    db: 'payload-$ENVIRONMENT'
   }]
 })
 EOF
